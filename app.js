eee53f8c756a482da09bd185637705e857a0b6bb
deprov447 <ac.10edu@gmail.com>
2020-08-11 10:29:40 +0530

    completed

--- app.js
+++ app.js
@@ -1,88 +1,121 @@
-var express                =     require("express"),
-    mongoose               =     require("mongoose"),
-    passport               =     require("passport"),
-    bodyParser             =     require("body-parser"),
-    User                   =     require("./models/user.js"),
-    LocalPass              =     require("passport-local"),
-    passportLocalMongoose  =     require("passport-local-mongoose");
+var express = require("express"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  // alerter= require("alert-node")
+  User = require("./models/user.js"),
+  LocalPass = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
+  
+  var JSAlert= require("js-alert")
+ 
 
+mongoose.connect("mongodb://localhost/auth_demo_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect("mongodb://localhost/auth_demo_app",{useNewUrlParser: true, useUnifiedTopology: true});
+var app = express();
+app.set("view engine", "ejs");
 
-var app= express();
-app.set("view engine","ejs")
+// app.use(morgan('combined'))
 
-
-app.use(require("express-session")({
-    secret: "HAshKeybyMe",
+app.use(
+  require("express-session")({
+    secret: "HashKeyforPassword",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
-passport.use(new LocalPass(User.authenticate()))
+passport.use(new LocalPass(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /////////////////
 //ROUTES
 /////////////////
 
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-app.get("/",function(req,res){
-    res.render("home");
-})
+app.get("/secret", isLoggedIn, function (req, res) {
+  res.render("secret");
+});
 
-app.get("/secret",isLoggedIn,function(req,res){
-    res.render("secret");
-})
+// app.post("/register", function (req, res) {
+//   User.register(
+//     new User({ username: req.body.username }),
+//     req.body.password,
+//     function (err, user) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       {
+//         passport.authenticate("local")(req, res, function () {
+//           res.redirect("/");
+//         });
+//       }
+//     }
+//   );
+// });
 
-app.get("/register",function(req,res){
-    res.render("register");
-})
+app.get("/login", function (req, res) {
+  res.render("login");
+});
 
-app.post("/register",function(req,res){
-    User.register(new User({username: req.body.username}), req.body.password,function(err,user){
-        if(err){
-            console.log(err);
-        }
-        {
-            passport.authenticate("local")(req,res,function(){
-                res.redirect("/");
-            })
-        }
-    })
-})
-
-app.get("/login",function(req,res){
-    res.render("login")
-})
-
-app.post("/login", passport.authenticate("local",{
-    successRedirect: "/secret",
-    failureRedirect: "/login"
-}),function(req,res){
-})
-
-app.get("/logout",function(req,res){
-    req.logout();
-    res.redirect("/");
-})
-
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated())
+app.post('/login',function (req, res) {
+  User.findOne({username: req.body.username},function(err, user){
+    if(user)
     {
-        return next();
+      console.log("user exist")
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/");
+      });
     }
-    res.redirect("/login");
+    else
+    {
+      console.log("user doesnt exist")
+      //////////
+        User.register(
+          new User({ username: req.body.username }),
+          req.body.password,
+          function (err, user) {
+            if (err) {
+              console.log(err);
+            }
+            {
+              passport.authenticate("local")(req, res, function () {
+                res.redirect("/secret");
+              });
+            }
+          }
+        );
+      //////////
+      res.redirect("/login");
+    }
+  })
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
-app.listen(1234,function(){
-    console.log("AUTH STARTED");
-    console.log("Go to localhost:1234/")
-})
+app.listen(1234, function () {
+  console.log("AUTH STARTED");
+  console.log("Go to localhost:1234/");
+});
