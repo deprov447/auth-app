d3662fc6c22db89c81dd554e13ecd332116f672a
deprov447 <ac.10edu@gmail.com>
2020-08-11 11:15:51 +0530

    finished

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ var express = require("express"),
   passport = require("passport"),
   bodyParser = require("body-parser"),
   morgan = require("morgan"),
-  // alerter= require("alert-node")
   User = require("./models/user.js"),
   LocalPass = require("passport-local"),
   passportLocalMongoose = require("passport-local-mongoose");
   
-  var JSAlert= require("js-alert")
- 
-
 mongoose.connect("mongodb://localhost/auth_demo_app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,7 +15,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app", {
 var app = express();
 app.set("view engine", "ejs");
 
-// app.use(morgan('combined'))
+app.use(morgan('dev'))
 
 app.use(
   require("express-session")({
@@ -37,6 +33,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req,res,next){
+  res.locals.currUser= req.user;
+  next();
+})
+
+
 /////////////////
 //ROUTES
 /////////////////
@@ -49,23 +51,6 @@ app.get("/secret", isLoggedIn, function (req, res) {
   res.render("secret");
 });
 
-// app.post("/register", function (req, res) {
-//   User.register(
-//     new User({ username: req.body.username }),
-//     req.body.password,
-//     function (err, user) {
-//       if (err) {
-//         console.log(err);
-//       }
-//       {
-//         passport.authenticate("local")(req, res, function () {
-//           res.redirect("/");
-//         });
-//       }
-//     }
-//   );
-// });
-
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -76,13 +61,12 @@ app.post('/login',function (req, res) {
     {
       console.log("user exist")
       passport.authenticate("local")(req, res, function () {
-        res.redirect("/");
+        res.redirect("/secret");
       });
     }
     else
     {
       console.log("user doesnt exist")
-      //////////
         User.register(
           new User({ username: req.body.username }),
           req.body.password,
@@ -97,8 +81,7 @@ app.post('/login',function (req, res) {
             }
           }
         );
-      //////////
-      res.redirect("/login");
+      res.redirect("/secret");
     }
   })
 });
